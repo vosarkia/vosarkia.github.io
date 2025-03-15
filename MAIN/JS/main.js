e00455457416304c4a7853eee7b9d42efa41721a
Robin Friedli <marius.friedli@gmail.com>
2025-03-15 20:59:25 +0100

    JS

---
+++ MAIN/JS/main.js
@@ -0,0 +1,11 @@
+﻿document.querySelectorAll('.nav-bar li').forEach(function(menuItem) {
+    menuItem.addEventListener('click', function(event) {
+        event.preventDefault();
+        console.log("Menu clicked!");  // Debugging line
+        
+        const dropdown = this.querySelector('.dropdown');
+        if (dropdown) {
+            dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+        }
+    });
+});
